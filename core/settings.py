e5f2b22c0e8c261a910b8745d8760cfd1e70c1b9
tinotz-max <stristanzarazaga@escuelasproa.edu.ar>
2026-06-29 12:29:32 -0300

    Chore: Update ALLOWED_HOSTS for web deployment

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a*npd-jl%c!2##5dmrxw96)-m$vtph!-gk4sadp$1)zeo(stxc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tinotzmax.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
